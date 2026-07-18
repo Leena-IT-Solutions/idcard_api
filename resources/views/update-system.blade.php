23ca6e060fce142c51ed151819c2665d05b7466b
sandeep8558 <sandeep198558@gmail.com>
2026-07-18 16:55:38 +0530

    Create blank Update System page and add sidebar link above Profile

---
+++ resources/views/update-system.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Update System') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-3xl p-6 text-gray-900 dark:text-gray-100">
+                {{ __('Update System page is under construction.') }}
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
+Route::view('update-system', 'update-system')
+    ->middleware(['auth'])
+    ->name('update-system');
+
 Route::middleware(['auth'])->group(function () {
     Route::get('/git-info', [\App\Http\Controllers\GitUpdaterController::class, 'info']);
     Route::post('/git-update', [\App\Http\Controllers\GitUpdaterController::class, 'update']);
