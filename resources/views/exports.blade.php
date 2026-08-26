84916b9bf0ec875668e097edd94b485b959e66e6
sandeep8558 <sandeep198558@gmail.com>
2026-08-26 21:28:16 +0530

    style(exports): switch Export & Print Center to full-width responsive layout

--- resources/views/exports.blade.php
+++ resources/views/exports.blade.php
@@ -17,8 +17,8 @@
         </div>
     </x-slot>
 
-    <div class="py-8">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-6">
+    <div class="py-6 sm:py-8 w-full">
+        <div class="w-full px-4 sm:px-6 lg:px-8 space-y-6">
             <livewire:exports.index />
         </div>
     </div>
